b201eed358f31b60ee6fe6675f0514de9957e043
emrenizamoglu <emzam2012@gmail.com>
2020-10-26 17:24:33 +0300

    Foodmodel and food json created

---
+++ lib/models/food_model.dart
@@ -0,0 +1,20 @@
+
+class FoodModel{
+  String type;
+  String name;
+  String description;
+  int price;
+  String link;
+
+  FoodModel({this.type, this.name, this.description,  this.price, this.link});
+
+  factory FoodModel.fromJson(Map<String, dynamic> parsedJson){
+    return FoodModel(
+      type: parsedJson['type'],
+      name: parsedJson['name'],
+      description: parsedJson['description'],
+      price: parsedJson['price'],
+      link: parsedJson['link'],
+    );
+  }
+}
