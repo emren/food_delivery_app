cd632d3127cb77550a5e0587a6130ac209a36883
emrenizamoglu <emzam2012@gmail.com>
2020-10-30 13:01:55 +0300

    Dismissible dialog added to BasketCard

--- lib/widgets/basket_card.dart
+++ lib/widgets/basket_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/models/basket_provider.dart';
-import 'package:food_delivery_app/models/food_provider.dart';
 import 'package:food_delivery_app/services/size_config.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -13,28 +12,122 @@ class BasketCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     BasketProvider basketProvider = Provider.of<BasketProvider>(context);
-    return ListTile(
-      leading: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-              color: Colors.deepOrangeAccent,
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image:
-                      NetworkImage(basketProvider.basketList[index].model.link),
-                  fit: BoxFit.cover)),
-        ),
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red,
       ),
-      title: Text(basketProvider.basketList[index].model.name,
+      onDismissed: (direction) {
+        basketProvider.removeFromBasket(basketProvider.basketList[index]);
+      },
+      confirmDismiss: (DismissDirection direction) async {
+        final bool res = await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              title: Text(
+                'Confirm',
+                style: GoogleFonts.titilliumWeb(
+                  textStyle: TextStyle(
+                      color: Colors.green[800],
+                      fontSize: 2 * SizeConfig.textMultiplier,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              content: Text(
+                'Do you want to delete this basket item?',
+                style: GoogleFonts.titilliumWeb(
+                  textStyle: TextStyle(
+                      color: Colors.green[800],
+                      fontSize: 2 * SizeConfig.textMultiplier,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: Text(
+                    'DELETE',
+                    style: GoogleFonts.titilliumWeb(
+                      textStyle: TextStyle(
+                          color: Colors.green[800],
+                          fontSize: 2 * SizeConfig.textMultiplier,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: Text(
+                    'CANCEL',
+                    style: GoogleFonts.titilliumWeb(
+                      textStyle: TextStyle(
+                          color: Colors.green[800],
+                          fontSize: 2 * SizeConfig.textMultiplier,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+        return res;
+      },
+      child: ListTile(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        basketProvider.basketList[index].model.link),
+                    fit: BoxFit.cover)),
+          ),
+        ),
+        title: Text(
+          basketProvider.basketList[index].model.name,
           style: GoogleFonts.titilliumWeb(
             textStyle: TextStyle(
                 color: Colors.green[800],
                 fontSize: 2 * SizeConfig.textMultiplier,
                 fontWeight: FontWeight.bold),
-          )),
+          ),
+        ),
+        trailing: Container(
+          width: 100,
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'qty: ' + basketProvider.basketList[index].quantity.toString(),
+                style: GoogleFonts.titilliumWeb(
+                  textStyle: TextStyle(
+                      color: Colors.green[800],
+                      fontSize: 2 * SizeConfig.textMultiplier,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                "₺ " + basketProvider.basketList[index].amount.toString(),
+                style: GoogleFonts.titilliumWeb(
+                  textStyle: TextStyle(
+                      color: Colors.green[800],
+                      fontSize: 2 * SizeConfig.textMultiplier,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
